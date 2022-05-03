63fd8c3f8e4d7b36c0ae6a1c516733d99a785b6d
CarlosAlexanderA <99810632+CarlosAlexanderA@users.noreply.github.com>
2022-05-03 11:51:14 -0500

    Delete pr_proyecto_001 directory

--- pr_proyecto_001/scripts/show_container.js
+++
@@ -1,9 +0,0 @@
-function ShowHomepage(){
-    
-    document.getElementById('show_list').style.display='none';
-    document.getElementById('show_homepage').style.display='flex';
-}
-function ShowList(){
-    document.getElementById('show_homepage').style.display='none';
-    document.getElementById('show_list').style.display='flex';
-}
